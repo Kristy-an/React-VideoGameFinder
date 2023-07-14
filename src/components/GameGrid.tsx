1c7e013a5478c3d3062a172eb37909e44a6e8bc9
Hongyan Zhao <hzhao292@wisc.edu>
2023-07-14 10:05:33 -0500

    DisplayCriticScore

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,21 @@
-import { useEffect, useState } from "react"
-import apiClient from "../services/api-client";
-import { Text } from "@chakra-ui/react";
 
-interface Game {
-    id:number;
-    name:string;
-}
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
-interface FetchGameResponse {
-    count: number;
-    results: Game[];
-}
+
 const GamGrid = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
+    const {games, error} = useGames();
 
-    useEffect(() => {
-        apiClient.get<FetchGameResponse>('/games')
-            .then(res => setGames(res.data.results))
-            .catch(err => setError(err.message));
-    })
 
     return(
         <>
             {error && <Text>{error}</Text>}
-            <ul>
-                {games.map(game => <li key={game.id}>{game.name}</li>)}
-            </ul>
+            <SimpleGrid column={{sm:1, md:2, lg:3, xl:5}} padding='10px' spacing={10}>
+                {games.map(game => (
+                    <GameCard key={game.id} game={game}/>
+                ))}
+            </SimpleGrid>
         </>
     )
 }
